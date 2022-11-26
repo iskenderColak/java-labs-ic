7bb3189b0dbf606f38236bdd2358badbc7d9f09a
iskenderColak <iclk84@gmail.com>
2022-11-26 21:36:05 +0300

    Variables && Operators

---
+++ src/com/icolak/lab01_variables_operators/DigitSum.java
@@ -0,0 +1,15 @@
+package com.icolak.lab01_variables_operators;
+
+public class DigitSum {
+
+    public static void main(String[] args) {
+
+        int number = 29814; // 2 + 9 + 8 + 1 + 4 = 24
+
+
+        int result = 24;
+
+
+        System.out.println("Result is: " + result);
+    }
+}
---
+++ src/com/icolak/lab01_variables_operators/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package com.icolak.lab01_variables_operators;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        System.out.println("Happy Mother's Day!");
+
+    }
+
+
+}
